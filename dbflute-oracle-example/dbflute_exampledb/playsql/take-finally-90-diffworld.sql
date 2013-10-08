30da014426837446bd56e9a0c715bd4cd6be972e
jflute <dbflute@gmail.com>
2013-10-08 21:40:36 +0900

    Refactor Oracle example

--- dbflute-oracle-example/dbflute_exampledb/playsql/take-finally-90-diffworld.sql
+++ dbflute-oracle-example/dbflute_exampledb/playsql/take-finally-90-diffworld.sql
@@ -1,1 +1,4 @@
 -- #df:checkEnv(diffworld)#
+
+alter table MEMBER modify (MEMBER_ACCOUNT VARCHAR(80))
+;
--- dbflute-oracle-example/dbflute_nextexampledb/_project.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export ANT_OPTS=-Xmx512m
-
-export DBFLUTE_HOME=../mydbflute/dbflute-1.0.5A
-
-export MY_PROJECT_NAME=nextexampledb
-
-export MY_PROPERTIES_PATH=build.properties
