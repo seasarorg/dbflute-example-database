457efe7b489f07657457c48e4b836f4ef07cb8ff
jflute <dbflute@gmail.com>
2014-04-29 23:34:13 +0900

    adjust

---
+++ dbflute-oracle-example/dbflute_exampledb/nextschema-renewal.bat
@@ -0,0 +1,27 @@
+@echo off
+
+setlocal
+%~d0
+cd %~p0
+call _project.bat
+
+set DBFLUTE_ENVIRONMENT_TYPE=nextschema
+
+echo /nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn
+echo ...Calling the ReplaceSchema task
+echo nnnnnnnnnn/
+call %DBFLUTE_HOME%\etc\cmd\_df-replace-schema.cmd %MY_PROPERTIES_PATH%
+
+echo /nnnnnnnnnnnnnnnnnnnnnnn
+echo ...Calling the JDBC task
+echo nnnnnnnnnn/
+call %DBFLUTE_HOME%\etc\cmd\_df-jdbc.cmd %MY_PROPERTIES_PATH%
+
+echo /nnnnnnnnnnnnnnnnnnnnnnnnnnn
+echo ...Calling the Document task
+echo nnnnnnnnnn/
+call %DBFLUTE_HOME%\etc\cmd\_df-doc.cmd %MY_PROPERTIES_PATH%
+
+if "%pause_at_end%"=="y" (
+  pause
+)
