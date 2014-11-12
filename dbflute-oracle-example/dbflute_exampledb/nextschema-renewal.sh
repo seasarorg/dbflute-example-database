43b2629d40e48fbb9696b66bad7bb7adb888c8d9
jflute <dbflute@gmail.com>
2014-11-13 01:19:58 +0900

    (DBFLUTE-1167){Java}: MySQLやPostgreSQLでの一件検索でfetchSize調整して大量検索阻止オプション

--- dbflute-oracle-example/dbflute_exampledb/nextschema-renewal.sh
+++ dbflute-oracle-example/dbflute_exampledb/nextschema-renewal.sh
@@ -14,11 +14,19 @@ echo "nnnnnnnnnn/"
 sh $DBFLUTE_HOME/etc/cmd/_df-replace-schema.sh $MY_PROPERTIES_PATH
 taskReturnCode=$?
 
+if [ $taskReturnCode -ne 0 ];then
+  exit $taskReturnCode;
+fi
+
 echo "/nnnnnnnnnnnnnnnnnnnnnnn"
 echo "...Calling the JDBC task"
 echo "nnnnnnnnnn/"
 sh $DBFLUTE_HOME/etc/cmd/_df-jdbc.sh $MY_PROPERTIES_PATH
 taskReturnCode=$?
+
+if [ $taskReturnCode -ne 0 ];then
+  exit $taskReturnCode;
+fi
 
 echo "/nnnnnnnnnnnnnnnnnnnnnnnnnnn"
 echo "...Calling the Document task"
@@ -29,3 +37,5 @@ taskReturnCode=$?
 if [ $taskReturnCode -ne 0 ];then
   exit $taskReturnCode;
 fi
+
+unset DBFLUTE_ENVIRONMENT_TYPE
